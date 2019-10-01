9ca695e52b27ed0fa8f0f10c4067ab347bfb96f6
Nicholas Werner <nswerner@ucdavis.edu>
2019-10-01 15:17:00 -0400

    Write deleteDeal(id) Action

--- src/actions.js
+++ src/actions.js
@@ -1,10 +1,20 @@
 export const CREATE_DEAL = 'CREATE_DEAL';
+export const DELETE_DEAL = 'DELETE_DEAL';
 
 export function createDeal(deal) {
   return {
     type: CREATE_DEAL,
     payload: {
       deal
+    }
+  }
+}
+
+export function deleteDeal(id) {
+  return {
+    type: DELETE_DEAL,
+    payload: {
+      id
     }
   }
 }
