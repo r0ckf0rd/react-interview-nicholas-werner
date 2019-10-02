ca3684e5c9d0108e4711e18d80bca6bce9b04b2c
Nicholas Werner <nswerner@ucdavis.edu>
2019-10-02 14:23:47 -0400

    Add Modify Deal Action

--- src/actions.js
+++ src/actions.js
@@ -1,5 +1,6 @@
 export const CREATE_DEAL = 'CREATE_DEAL';
 export const DELETE_DEAL = 'DELETE_DEAL';
+export const MODIFY_DEAL = 'MODIFY_DEAL';
 
 export function createDeal(deal) {
   return {
@@ -15,6 +16,16 @@ export function deleteDeal(id) {
     type: DELETE_DEAL,
     payload: {
       id
+    }
+  }
+}
+
+
+export function modifyDeal(deal) {
+  return {
+    type: MODIFY_DEAL,
+    payload: {
+      deal
     }
   }
 }
